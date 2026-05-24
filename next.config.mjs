8bcdebaa703d8346adf6c2e932237cfe77ef3bdc
Aditya Garud <garudaditya079@gmail.com>
2026-05-25 00:15:18 +0530

    next config for v16 (remove eslint key, add turbopack root)

--- next.config.mjs
+++ next.config.mjs
@@ -1,51 +1,17 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   trailingSlash: true,
-  distDir: 'out',
-  eslint: {
-    ignoreDuringBuilds: true,
+  distDir: "out",
+  images: {
+    unoptimized: true,
+  },
+  turbopack: {
+    root: import.meta.dirname,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
-}
-
-mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
 }
 
 export default nextConfig
